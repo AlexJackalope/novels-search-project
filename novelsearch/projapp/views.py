bc2f7504387b6298d0d2c20ed5ff68431ab7631b
Alexandra Zaytseva <alexandrazaitzewa@yandex.ru>
2022-05-14 21:47:56 +0500

    Albums start

--- novelsearch/projapp/views.py
+++ novelsearch/projapp/views.py
@@ -16,6 +16,34 @@ classifier = Classifier.Classifier(games_info_df)
 pop_tags_to_links = classifier.get_tags()
 filtration = Filtration.Filtration(pop_tags_to_links)
 
+first_visit = [
+    'https://gbpatch.itch.io/our-life-nf',
+    'https://oracleandbone.itch.io/a-summers-end',
+    'https://zephyo.itch.io/you-left-me',
+    'https://laundrybear.itch.io/morticians-tale',
+    'https://batensan.itch.io/pizzaro-project-deep-dish',
+    'https://via01.itch.io/dont-open-your-eyes',
+    'https://nomnomnami.itch.io/contract-demon',
+    'https://fymm-game.itch.io/ddp'
+]
+insomnia = [
+    'https://hroft32.itch.io/rusty-punk',
+    'https://klace.itch.io/windsofchange',
+    'https://cockhole.itch.io/9-22',
+    'https://deckerwolf.itch.io/super-taco-crew'
+]
+cute_nightmare = [
+    'https://saikono.itch.io/tiny-bunny',
+    'https://zephyo.itch.io/you-left-me',
+    'https://clowndream.itch.io/hikeback',
+    'https://garage-heathen.itch.io/whos-lila',
+    'https://nikita-kryukov.itch.io/pmkm',
+    'https://via01.itch.io/dont-open-your-eyes'
+]
+
+albums = {'First visit': first_visit,
+          'Insomnia': insomnia,
+          'Cute nightmare': cute_nightmare}
 
 class TagForm(forms.Form):
     OPTIONS = tuple([(tag, tag) for tag in pop_tags_to_links])
@@ -39,4 +67,14 @@ def index(req):
                   'projapp/index.html',
                   {'games': selected_games,
                    'tags': sorted(pop_tags_to_links.keys()),
-                   'form': form})
+                   'albums': albums,
+                   'tag_form': form,
+                   'albums': albums})
+
+
+def album(req, album_name):
+    album_games = [link_to_gi[link] for link in albums[album_name]]
+    return render(req,
+                  'projapp/album.html',
+                  {'games': album_games,
+                   'album_name': album_name})
